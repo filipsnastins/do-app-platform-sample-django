51eb979ac0e9d16d9ba92a3e11c67306efc0c36e
Filips Nastins <filips.nastins@billogram.com>
2023-04-07 11:38:22 +0200

    add CSRF_TRUSTED_ORIGINS

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,6 +34,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
+CSRF_TRUSTED_ORIGINS = ["https://*.{ALLOWED_HOSTS}"]
 
 # Application definition
 
